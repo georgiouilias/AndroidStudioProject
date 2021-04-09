46109dd6fa1a75aa575079627777b09d563c08a4
GeorgiouIlias <66827818+georgiouilias@users.noreply.github.com>
2021-04-09 20:18:14 +0300

    InsertMain-Icons-InsertFolder
    1) new images for nav menu
    2) insert sport,athlete,event,team created 3) insert main fragment has buttons

---
+++ app/src/main/java/com/example/androidstudioproject/ui/insert/InsertTeam.java
@@ -0,0 +1,66 @@
+package com.example.androidstudioproject.ui.insert;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.androidstudioproject.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link InsertTeam#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class InsertTeam extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public InsertTeam() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment InsertTeam.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static InsertTeam newInstance(String param1, String param2) {
+        InsertTeam fragment = new InsertTeam();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_insert_team, container, false);
+    }
+}
